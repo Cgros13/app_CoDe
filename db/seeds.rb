2f386e48e82e72ae0560c1b9554ac2906abf3b4f
JohanPer <johan.perrusson@hotmail.fr>
2022-11-29 16:45:49 +0100

    authenticate user + turbo disable + seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.destroy_all
+
+User.create!(email: 'Allan@example.com', password: 'azerty')
+User.create!(email: 'Cgros@example.com', password: 'azerty')
+User.create!(email: 'Jojo@example.com', password: 'azerty')
+User.create!(email: 'Ines@example.com', password: 'azerty')
