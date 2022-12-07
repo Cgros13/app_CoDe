133f39f5eaca3c43e748b068a839cd2f8dfff41d
Charles Gros <charles13520@gmail.com>
2022-12-07 16:32:43 +0100

    trop de truc

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,14 @@
 class PagesController < ApplicationController
   def home
+    @visits = current_user.visits
+
+    case params[:period]
+    when "day"
+      @visits = @visits.where("created_at > ?", 1.day.ago)
+    when "week"
+      @visits = @visits.where("created_at > ?", 1.week.ago)
+    when "month"
+      @visits = @visits.where("created_at > ?", 1.month.ago)
+    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def green_data(visits)
+    count = visits.compact.count do |visit|
+      visit.green
+    end
+    (count.to_f / visits.count.to_f).round(2) * 100
+  end
 end
